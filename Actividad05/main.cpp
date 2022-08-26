aaa89c4c873eff9d43f6a74d99d77346a1884b52
alex <a01721287@itesm.mx>
2022-08-25 21:22:57 -0500

    avance act5

--- Actividad05/main.cpp
+++ Actividad05/main.cpp
@@ -4,44 +4,61 @@
 using namespace std;
 
 
-vector<int> solve(vector<int> up, vector<int> down,vector<int> desired){
-    for (int i = 0; i < up.size(); i++){
-        cout<<up[i]<<" ";
+string resolver(int n1,int n2,vector<int> T1, vector<int> T2,vector<int> sal){
+    bool solve[n1][n2];
+    for (int i = 0; i < n1; i++){
+        for (int j = 0; j < n2; j++){
+            if (i==j==0||(i==n1&&j==n2)){
+                continue;
+            }
+            solve[i,j] = solve[i,j-1]&&T2[j]==sal[j];
+        }
+    }
+    
+
+    solve[n1,n2] = sal[n1+n2]==T1[n1]&&T2[n1-1,n2]||sal[n1+n2]==T2[n2]&&solve(n1,n2-1);
+
+/*     for (int i = 0; i < T1.size(); i++){
+        cout<<T1[i]<<" ";
     }
     cout<<endl;
-    for (int i = 0; i < down.size(); i++){
-        cout<<down[i]<<" ";
+    for (int i = 0; i < T2.size(); i++){
+        cout<<T2[i]<<" ";
     }
     cout<<endl;
-    for (int i = 0; i < desired.size(); i++)
+    for (int i = 0; i < sal.size(); i++)
     {
-        cout<<desired[i]<<" ";
+        cout<<sal[i]<<" ";
     }
     cout<<endl;
 
-    
-    return{0,0};
+     */
+    return "possible";
 }
 
 int main(){
-    int n,m,tmp;
-    cin>>n>>m;
-    vector<int> up, down,desired;
-    for (int i = 0; i < n; i++){
-        cin>>tmp;
-        up.push_back(tmp);
+    int n1,n2,tmp;
+    while (true){
+        cin>>n1>>n2;
+        if (n1==0&&n2==0){
+            exit(1);
+        }
+        vector<int> T1, T2,sal;
+        for (int i = 0; i < n1; i++){
+            cin>>tmp;
+            T1.push_back(tmp);
+        }
+        for (int i = 0; i < n2; i++){
+            cin>>tmp;
+            T2.push_back(tmp);
+        }
+        for (int i = 0; i < n1+n2; i++){
+            cin>>tmp;
+            sal.push_back(tmp);
+        }
+        
+        resolver(n1,n2,T1, T2, sal);
     }
-    for (int i = 0; i < m; i++){
-        cin>>tmp;
-        down.push_back(tmp);
-    }
-
-    for (int i = 0; i < n+m; i++){
-        cin>>tmp;
-        desired.push_back(tmp);
-    }
-    
-    solve(up, down,desired);
 }
 
 /*
