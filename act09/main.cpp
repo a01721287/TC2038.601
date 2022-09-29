61a9ffdde0b8ada6b00503a68624233d5ee9326e
alex <a01721287@itesm.mx>
2022-09-28 21:09:14 -0500

    acabar act09

--- act09/main.cpp
+++ act09/main.cpp
@@ -1,71 +1,73 @@
+/* Rocky the beagle
+Alejandro Hernandez A01721287 */
 #include <iostream>
-#include <queue> //priority list
-#include <vector> 
-#include <climits> //para intMAX 
-#include <utility> //para pair
+#include <queue>
+#include <vector>
+#include <iterator>
+#include <climits>
+#include <utility>
 
 using namespace std;
 
 #define INF INT_MAX
-#define Edge pair <int,int> // a dónde llega y su costo
-#define Graph vector<vector<Edge> > //Vector de pares
-#define Vertex pair<int,int> //Distancia (costo), ID
+#define Edge pair<int, int> // A donde llego y su costo
+#define Graph vector< vector<Edge> > // Grafo rep. en una lista de adjacencias
+#define Vertex pair<int, int> // Distancia (Costo), ID
 
-//Complejidad O(n log n)
-vector<int> dijkstra(Graph &G, int src){
-    vector<int> dist(G.size(), INF);
-    dist[src] = 0;
-    
-    priority_queue<Vertex, vector<Vertex>, greater<Vertex> >queue;
-    Vertex vs(0,src);
-    queue.push(vs);
-    while(!queue.empty()){
-        int u = queue.top().second;
-        queue.pop();
-
-        for (int i = 0; i < G[u].size(); i++){
-            Edge e = G[u][i];
-            int v = e.first;  //Hacia dónde  llego
-            int w = e.second; //con qué costo llego
-            if (dist[v]>dist[u]+w){
-                dist[v] = dist[u]+w;
-                Vertex vtx(dist[v], v);
-                queue.push(vtx);
-            }
-            
-        }
-        
-    }
-
-
-    return dist;
+/* void print(Graph &G){
+	for (int i=0; i<G.size(); i++){
+		cout << (i+1) << " ==> ";
+		for (int j=0; j<G[i].size(); j++){
+			cout << "(" << (G[i][j].first+1) << "," 
+				 << G[i][j].second << ") ";
+		}
+		cout << endl;
+	}
+} */
+//Complejidad: O(n log n)
+vector<int> dijkstra(Graph &G, int source){
+	vector<int> dist(G.size(), INF);
+	dist[source] = 0;
+	priority_queue<Vertex, vector<Vertex>, greater<Vertex> > queue;
+	Vertex vs(0, source);
+	queue.push(vs);
+	while (!queue.empty()){
+		int u = queue.top().second;
+		queue.pop();
+		for (int i=0; i<G[u].size(); i++){
+			Edge e = G[u][i];
+			int v = e.first; // Hacia donde llego
+			int w = e.second; // Con que costo llego
+			if (dist[v] > dist[u]+w){
+				dist[v] = dist[u]+w;
+				Vertex vtx(dist[v], v);
+				queue.push(vtx);
+			}
+		}
+	}
+	return dist;
 }
-
-void print(Graph &G){
-    for (int i = 0; i < G.size(); i++){
-        cout<< i+1 <<"-->";
-        for (int j = 0; j < G[i].size(); j++){
-            cout<<"("<<G[i][j].first<<","<<G[i][j].second<<")";
-        }
-        cout<<endl;
-    }
-}
-
+//Complejidad: O(n^2)
 int main(){
-    vector<char> result;
+    vector<char> results;
     int cases;
-    cin >> cases;
-    for (int i = 0; i < cases; i++){
+    cin>>cases;
+    for (int z = 0; z < cases; z++){
         int nodes, edges, source, u, v, w;
-        char tmp1, tmp2;
+        char tmp1,tmp2;
+        source = 1; //Todos salen de A
         cin >> nodes >> edges;
-        source = 1;
-
         Graph G(nodes);
+
+        int check = false;
         for (int i=0; i<edges; i++){
             cin >> tmp1 >> tmp2 >> w;
             u = tmp1-64;
             v = tmp2-64;
+            if (u == 1 || v ==1){
+                check = true;
+            }
+
             // Arco de u->v con un costo w
             Edge edge1(v-1, w);
             G[u-1].push_back(edge1);
@@ -73,32 +75,43 @@ int main(){
             Edge edge2(u-1, w);
             G[v-1].push_back(edge2);
         }
+        if (!check){
+            results.push_back('B');
+            continue;
+        }
 
-        // print(G);
+        //print(G);
         vector<int> distances = dijkstra(G, source-1);
-        
-        // cout << "Distancias: " << endl;
-        int highest=0;
-        for (int i=0; i<nodes; i++){
-            if (i != source-1){
-                // cout << source << "-->" << (i+1) << " : ";
-                if (distances[i] == INF){
-                    // cout << "INF" << endl;
-                    highest = i;
-                }
-                else if(distances[i]>highest){
-                    highest = i+1;
-                    // cout << distances[i] << endl;
-                }else{
-                    highest = 2;
-                }
+        /* for (int i = 0; i < distances.size(); i++){
+            cout<<distances[i]<<", ";
+        }
+        cout<<endl; */
+        int highest = 0;
+        int highestCost= 0;
+        for (int i = 0; i < distances.size(); i++){
+            if (distances[i]>highestCost && distances[i]!=INF){
+                highestCost = distances[i];
+                highest = i;
+            }else if(distances[i]==highestCost&&i<highest){
+                highest = i;
             }
         }
-        result.push_back(highest+64);
+        results.push_back(char(highest+65));
+        /* 
+        for (int i=0; i<nodes; i++){
+            if (i != source-1){
+                cout << source << "-->" << (i+1) << " : ";
+                if (distances[i] == INF){
+                    cout << "INF" << endl;
+                }
+                else{
+                    cout << distances[i] << endl;
+                }
+            }
+        } */
     }
-    
-    for (int i = 0; i < cases; i++){
-        cout<<"Case "<< i+1<<": "<<result[i]<<endl;
+    for (int i = 0; i < results.size(); i++){
+        cout<<"Case "<< i+1 <<": "<<results[i]<<endl;
     }
     
 }
@@ -121,7 +134,5 @@ A C 1
 A D 1
 B C 1
 B D 1
-C D 1
-
-
+C D  1
 */
