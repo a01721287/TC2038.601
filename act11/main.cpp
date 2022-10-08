511e2f47e1d595b2c2ae2971a9b4ea98c6500aa8
alex <a01721287@itesm.mx>
2022-10-08 18:31:34 -0500

    up

--- act11/main.cpp
+++ act11/main.cpp
@@ -1,6 +1,5 @@
-//Algoritmo de branch & bound
-//Requerimientos: Grafo completamente conectado de todos los nodos a todos
-//Propósito: Partir de un nodo y recorrer todo el grafo sin regresarte o pasar por un camino previamente cruzado hasta llegar otra vez al nodo origen.
+// Travelling Saleman Problem
+// TSP con Branch & Bound
 
 #include <iostream>
 #include <climits>
@@ -11,129 +10,203 @@
 using namespace std;
 
 struct Nodo{
-    int niv; //nivel del nodo adentro del arbol de búsqueda soluciones
-    int costoAcum;  //Costo acum hasta ese punto
-    int costoPos; //
-    int verticeAnterior;
-    int verticeActual;
-    bool visitados[MAX] = {false};
-    bool operator<(const Nodo &otro) const{
-        return costoPos >= otro.costoPos;
-    }
-
+	int niv; 			// Nivel del Nodo
+	int costoAcum;		// Costo Real Acumulado hasta ese nodo
+	int costoPos;		// Costo Posible por esa trayectoria
+	int verticeAnterior;
+	int verticeActual;
+	bool visitados[MAX] = {false};
+	bool operator<(const Nodo &otro) const{	// Para que la fila priorizada tenga priodad
+		return costoPos >= otro.costoPos;	// Costo Posible menor.
+	}
 };
 
-
-void calculaCostoPosible(Nodo &nodoActual, int matAdy[MAX][MAX], int n){
-    nodoActual.costoPos = nodoActual.costoAcum;
-    int costoObtenido;
-    for (int i = 1; i <= n; i++){
-        costoObtenido = INT_MAX;
-        //no he visitado al nodo i o es el último que visité
-        if (!nodoActual.visitados[i]|| i == nodoActual.verticeActual){
-            if (!nodoActual.visitados[i]){
-                for (int j = 1; j <= n; j++){
-                    if (i!=j&&(!nodoActual.visitados[i] || j==1)){
-                        costoObtenido = min(costoObtenido, matAdy[i][j]);
-                    }
-                }
-            }else{
-                for (int j = 1; j <=n; j++){
-                    costoObtenido = min(costoObtenido, matAdy[i][j]);
-                }
-                
-            }
-            
-        }
-        if (costoObtenido ==INT_MAX){
-            nodoActual.costoPos = INT_MAX;
-        }else{
-            nodoActual.costoPos += costoObtenido;
-        }
-    }
-
+void calculaCostoPosible(Nodo &nodoActual, int matAdj[MAX][MAX], int n){
+	nodoActual.costoPos = nodoActual.costoAcum;
+	int costoObtenido;
+	for (int i=1; i<=n; i++){
+		costoObtenido = INT_MAX;
+		// No he visitado al nodo i o es el Ãºltimo que visite,
+		if (!nodoActual.visitados[i] || i == nodoActual.verticeActual){
+			if (!nodoActual.visitados[i]){ // Al nodo i no lo he visitado
+				for (int j=1; j<=n; j++){
+					if (i!=j && (!nodoActual.visitados[j] || j == 1)){
+						costoObtenido = min(costoObtenido, matAdj[i][j]); 
+					}
+				}
+			}
+			else{	// El nodo i es el Ãºltimo visitado
+				for (int j=1; j<=n; j++){
+					if (!nodoActual.visitados[j]){
+						costoObtenido = min(costoObtenido, matAdj[i][j]);
+					}
+				}
+			}
+			if (costoObtenido != INT_MAX){
+				nodoActual.costoPos += costoObtenido;
+			}
+			else{
+				nodoActual.costoPos = INT_MAX;
+			}
+		}
+	}
 }
 
-void iniciaMat(int matAdy[MAX][MAX]){
-    for (int i = 0; i < MAX; i++){
-        matAdy[i][i] = 0;
-        for (int j = 1; j < MAX; j++){
-            matAdy[i][j] = matAdy[j][i] = INT_MAX;
+void printMat(int matAdj[MAX][MAX], int n){
+    for (int i = 0; i <= n; i++){
+        for (int j = 0; j <= n; j++){
+            cout<<matAdj[i][j]<<" ";
         }
-    }
-}
-
-void leeArcos(int matAdy[MAX][MAX], int m){
-    char a,b;   //Nodos conectados
-    int c;      //Costo de conexión
-    
-    for (int i = 0; i < m; i++){
-        cin>>a>>b>>c;
-        matAdy[a-'A'+1][b-'A'+1] = matAdy[b-'A'+1][a-'A'+1] = c;
+        cout<<endl;
     }
 }
 
-//Complejidad: O(2^n)
-int tsp(int matAdy[MAX][MAX], int n){
-    int costoOptimo = INT_MAX;
-    Nodo raiz;
-    raiz.niv = 0;
-    raiz.costoAcum = 0;
-    raiz.verticeActual = 1;
-    raiz.verticeAnterior = 0;
-    raiz.visitados[1] = true;
-    calculaCostoPosible(raiz, matAdy, n);
-    priority_queue<Nodo> pq;
-    pq.push(raiz);
-    /* while (!pq.empty()){
-        // Sacar de pq;
-        // Ver si el CostoPos < Costo Optimo
-        // SI si, generar todos los posibles hijos de este nodo
-        // Para cada hijo generar un nuevo nodo, actualizar los datos
-        // y cuando el nivel == n-2 calcular el costo real, y si este
-        // mejora el costo optimo ==> actualizarlo
-        // Si el nivel < n-2 .... checar si el costo posible es mejor
-        // que el costo optimo y lo metes a la pq.
-
-        Nodo sacar = pq.top();
-        if (sacar.costoPos < costoOptimo){
-            cout<<"generar cada hijos de nodo "<< sacar.niv<<endl;
-            
-            
-        }
-        pq.pop();
-
-
-        
-    } */
-    
-    return 0;
+void prpq(priority_queue<Nodo> pq){
+	priority_queue<Nodo> e = pq;
+	cout<<"queue: ";
+	while (!e.empty()){
+		cout<<e.top().costoPos<<", ";
+		e.pop();
+	}
+	cout<<endl;
 }
+
+int tsp(int matAdj[MAX][MAX], int n){
+    string path = "";
+	int costoOptimo = INT_MAX;
+	Nodo raiz;
+	raiz.niv = 0;
+	raiz.costoAcum = 0;
+	raiz.verticeActual = 1;
+	raiz.verticeAnterior = 0;
+	raiz.visitados[1] = true;
+	calculaCostoPosible(raiz, matAdj, n);
+	priority_queue<Nodo> pq;
+	pq.push(raiz);
+    // printMat(matAdj, n);
+    while (!pq.empty()){
+		// Sacar de pq;
+		// Ver si el CostoPos < Costo Optimo
+		// SI si, generar todos los posibles hijos de este nodo
+		// Para cada hijo generar un nuevo nodo, actualizar los datos
+		// y cuando el nivel == n-2 calcular el costo real, y si este
+		// mejora el costo optimo ==> actualizarlo
+		// Si el nivel < n-2 .... checar si el costo posible es mejor
+		// que el costo optimo y lo metes a la pq.
+
+		Nodo parent = pq.top();
+		if (parent.costoPos < costoOptimo){
+			for (int i = parent.verticeActual+1; i <= n; i++){
+				if (!parent.visitados[i]){
+					Nodo child;
+					child.niv = parent.niv + 1;
+					child.costoAcum = parent.costoAcum + matAdj[parent.verticeActual][i];
+					child.verticeActual = i;
+					child.verticeAnterior = parent.verticeActual;
+					copy(begin(parent.visitados), end(parent.visitados), begin(child.visitados));
+					child.visitados[i] = true;
+					calculaCostoPosible(child, matAdj, n);
+					// cout<<"SE CALCULÓ: "<<child.costoPos<<endl;
+
+					if (child.niv == n-2){
+						if (child.costoPos<costoOptimo){
+							costoOptimo = child.costoPos;
+						}
+					}else if(child.niv< n-2){
+						pq.push(child);
+					}
+				}
+			}
+			pq.pop();
+		}else{
+			pq.pop();
+		}
+
+
+        // Nodo tmp = pq.top();
+        // if (tmp.costoPos < costoOptimo){
+        //     for (int i = 1; i <= n; i++){
+        //         if (matAdj[tmp.verticeActual][i]!=0 && matAdj[tmp.verticeActual][i]<INT_MAX
+		// 			&& !tmp.visitados[i]){
+
+        //             Nodo child;
+        //             child.niv = tmp.niv + 1;
+        //             child.costoAcum = tmp.costoAcum + matAdj[tmp.verticeActual][i];
+	    //             child.verticeActual = i-1;
+	    //             child.verticeAnterior = tmp.verticeActual;
+		// 			child.visitados[i] = true;
+        //             calculaCostoPosible(child, matAdj, n);
+		// 			cout<<"SE CALCULÓ: "<<child.costoPos<<endl;
+        //             if (child.niv == n-2 && child.costoAcum<costoOptimo){
+        //                 costoOptimo = child.costoAcum;
+        //             }else if (child.niv < n-2 && child.costoPos<costoOptimo){
+		// 				cout<<"niv:"<<child.niv<<"acum:"<<child.costoAcum<<"actual:"<<child.verticeActual
+		// 				<<"antes:"<<child.verticeAnterior<<"cosPos:"<<child.costoPos<<endl;
+		// 				// cout<<" "<<child.verticeAnterior<<"-"<<child.verticeActual<<" ";
+		// 				pq.push(child);
+        //             }
+        //         }
+        //     }
+		// 	//Final de nivel
+		// 	pq.pop(); //Quitar el anterior de la fila priorizada
+		// 	prpq(pq); //Imprimir
+        // }else{
+		// 	pq.pop();
+		// }
+    }
+	return costoOptimo;
+}
+
+
+void iniciaMat(int matAdj[MAX][MAX]){
+	for (int i=0; i<MAX; i++){
+		matAdj[i][i] = 0;
+		for (int j=i+1; j<MAX; j++){
+			matAdj[i][j] = matAdj[j][i] = INT_MAX;
+		}
+	}
+}
+
+void leeArcos(int matAdj[MAX][MAX], int m){
+	char a, b; 	// De dÃ³nde a dÃ³nde en la trayectoria
+	int c; 		// Costo de la trayectoria
+	for (int i=0; i<m; i++){
+		cin >> a >> b >> c;
+		matAdj[a-'A'+1][b-'A'+1] = matAdj[b-'A'+1][a-'A'+1] = c;
+	}
+}
+
 
 int main(){
-    int n, m, result; //n=cant nodos, m = cant arcos
-    cin>>n,m;
-
-    int matAdy[MAX][MAX];
-    iniciaMat(matAdy);
-    leeArcos(matAdy, m);
-    result = tsp(matAdy, n);
-    if (result == INT_MAX){
-        cout<<"INF"<<endl;
-    }else{
-        cout<<result<<endl;
-    }
-    
-    return 0;
+	// n = Cantidad de nodos
+	// m = Cantidad de arcos
+	int n, m, result;
+	cin >> n >> m;
+	int matAdj[MAX][MAX];
+	iniciaMat(matAdj);
+	leeArcos(matAdj, m);
+	result = tsp(matAdj, n);
+	if (result == INT_MAX){
+		cout<<"INF"<<endl;
+	}else{
+		cout<<result<<endl;
+	}
+	
 }
 
 /*
-4 5 
-A B 5 
-A C 10 
-A D 8 
-B C 2 
+4 6
+A B 2
+A C 3
+A D 5
+B C 4
 C D 1
+B D 6
 
-
+4 5
+A B 5
+A C 10
+A D 8
+B C 2
+C D 1
 */
